c5f889e9cd7d2c0edae0664b90ec0fde924a1bdb
Wutthiphon Main PC <woodychgamer5588@gmail.com>
2022-04-25 20:20:28 +0700

    update chk user error

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -21,6 +21,7 @@ class AuthController extends Controller
         if($request->post('email')){
             $status = false;
             $permission = [];
+            $token = null;
 
             $userdata = DB::table('users')
             ->where('email',$request->post('email'))
@@ -116,11 +117,13 @@ class AuthController extends Controller
 
     protected function respondWithToken($token)
     {
-        return response()->json([
-            'access_token' => $token,
-            'token_type' => 'bearer',
-            'expires_in' => auth('api')->factory()->getTTL() * 60,
-        ]);
+        if($token){
+            return response()->json([
+                'access_token' => $token,
+                'token_type' => 'bearer',
+                'expires_in' => auth('api')->factory()->getTTL() * 60,
+            ]);
+        }
     }
 
     protected function guard()
